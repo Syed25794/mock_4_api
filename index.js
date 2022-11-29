4e820c2f89e5f97a201aabc74ec64ee54a58148d
Syed25794 <syedgulmohammad25794@gmail.com>
2022-11-29 09:12:22 +0530

    changed the port number accordingly to package.json file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('database.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8086 ;
+const port = process.env.PORT || 8080 ;
 
 server.use(middlewares);
 server.use(router);
